cb051fb112686f1aa4d661caabb18e48ce0b2a65
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-15 22:39:37 +0530

    Basic Template

--- a1-start/src/app/app.module.ts
+++ a1-start/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- a1-start/src/app/modules/shared/shared.module.ts
+++ a1-start/src/app/modules/shared/shared.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { NgmaterialModule } from '~/app/ngmaterial/ngmaterial.module';
 
-
+const components = [
+  HeaderComponent,
+  FooterComponent,
+];
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    HeaderComponent
+    ...components,
+  ],
+  exports: [
+    ...components
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgmaterialModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
