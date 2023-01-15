97430506450bc7098e247a17176976d976abc436
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-15 22:36:45 +0530

    Basic header footer

--- a1-start/src/app/modules/shared/shared.module.ts
+++ a1-start/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
