a887f5c4a97dc8b5a7058ccc95c1cc27fe78e846
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-12 21:28:56 +0530

    Few Changes

---
+++ SampleApp/src/app/components/inlinecomponent/inline.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-inline',
+    template: `
+    <h1>Welcome To Angular 15 Development</h1>
+    <h1>{{Message}}</h1>
+    <h3>{{Message}}</h3>
+`,
+    styles: [
+        'h1{font-weight:bold;font-style:italic;text-decoration:underline;color:blue}',
+        'h3{color:red}'
+    ]
+})
+export class InlineComponent {
+    Message: string = "This is Inline Angular Component.";
+}
