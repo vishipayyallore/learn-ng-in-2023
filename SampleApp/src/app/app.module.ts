906d0e33b299406b713bc031917359f5f3f3451b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-13 09:16:23 +0530

    Few Changes (#8)

--- SampleApp/src/app/app.module.ts
+++ SampleApp/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InlineComponent } from './components/inlinecomponent/inline.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InlineComponent
   ],
   imports: [
     BrowserModule,
