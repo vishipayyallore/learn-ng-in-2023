0e9628a6a40da3246b814141cadc2bac679ff83f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-04 22:57:43 +0530

    Code Refactoring

--- mediumclone/src/app/auth/auth.routes.ts
+++ mediumclone/src/app/auth/auth.routes.ts
@@ -1,6 +1,4 @@
 import { Route } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 
-export const registerRoutes: Route[] = [
-    { path: '', component: RegisterComponent },
-];
+export const registerRoutes: Route[] = [{ path: '', component: RegisterComponent }];
