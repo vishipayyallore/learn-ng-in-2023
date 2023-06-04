92c9b3111df4f605665350f2f6c6d86cae8dfb3b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-04 22:22:35 +0530

    Register Page

--- mediumclone/src/main.ts
+++ mediumclone/src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-    providers: [],
+    providers: [provideRouter(appRoutes)],
 });
