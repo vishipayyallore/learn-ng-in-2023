296b951289b3003c5eee5b6127bfadc0316e154b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-22 22:14:16 +0530

    Using Selectors

--- mediumclone/src/app/auth/components/register/register.component.ts
+++ mediumclone/src/app/auth/components/register/register.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RegisterRequestInterface } from '../../types/registerRequest.interface';
-import { Store } from '@ngrx/store';
-import { register } from '../../store/actions';
 import { RouterLink } from '@angular/router';
 import { selectIsSubmitting } from '@auth/store/selectors';
 import { AuthStateInterface } from '@auth/types/authState.interface';
+import { Store } from '@ngrx/store';
+import { register } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 
 @Component({
     selector: 'mc-register',
     templateUrl: './register.component.html',
     standalone: true,
-    imports: [ReactiveFormsModule, RouterLink],
+    imports: [ReactiveFormsModule, RouterLink, CommonModule],
 })
 export class RegisterComponent {
     form = this.fb.nonNullable.group({
