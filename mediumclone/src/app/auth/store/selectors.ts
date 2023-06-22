a2daa56380a7ae9ea55ebbbbb3ad13314e76b2d0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-22 21:47:15 +0530

    Update selectors.ts

--- mediumclone/src/app/auth/store/selectors.ts
+++ mediumclone/src/app/auth/store/selectors.ts
@@ -1,3 +1,6 @@
 import { AuthStateInterface } from '@auth/types/authState.interface';
+import { createSelector } from '@ngrx/store';
 
 export const selectFeature = (state: { auth: AuthStateInterface }) => state.auth;
+
+export const isSubmittingSelector = createSelector(selectFeature, (state) => state.isSubmitting);
