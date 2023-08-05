7fcbfc981bb01437efdfd7e092a176bddb658834
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 23:13:54 +0530

    Nav Bar

--- blog-app/src/app/core/core.module.ts
+++ blog-app/src/app/core/core.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 
-
-
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent
-  ]
+  declarations: [HeaderComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
