dc35719e352f407d450bca9de40e3620f148ef59
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 23:40:35 +0530

    Changes

--- blog-app/src/app/articles/articles.component.ts
+++ blog-app/src/app/articles/articles.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'sv-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+  styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  posts$: Observable<ScullyRoute[]> | undefined;
 
+  constructor(private scullyService: ScullyRoutesService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.scullyService.available$.pipe(
+      map((posts) => posts.filter((post) => post.title))
+    );
+  }
 }
