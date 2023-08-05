171ee64b01fd42a0e8c92ce9f5a4d27bb706c7ea
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 23:04:56 +0530

    Update app.module.ts

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
