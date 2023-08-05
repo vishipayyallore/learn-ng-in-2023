6a331214a30814d4e1946dddf18be301570e99f2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 22:53:56 +0530

    Refactoring

--- blog-app/src/app/shared/footer/footer.component.ts
+++ blog-app/src/app/shared/footer/footer.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sv-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   currentDate = new Date();
-
 }
