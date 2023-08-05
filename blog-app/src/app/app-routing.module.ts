38c4281ff3d26470d4188dda2cfa95bbe3d91907
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 23:16:30 +0530

    Update app-routing.module.ts

--- blog-app/src/app/app-routing.module.ts
+++ blog-app/src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [{ path: 'contact', component: ContactComponent }, { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  {
+    path: 'articles',
+    loadChildren: () =>
+      import('./articles/articles.module').then((m) => m.ArticlesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
