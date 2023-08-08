3d0656faed2b08e0357773c2417c4d83baf1d582
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-09 00:06:16 +0530

    Product List

--- shop-app/src/app/products/product-list/product-list.component.ts
+++ shop-app/src/app/products/product-list/product-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'sv-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+    selector: 'sv-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
-
+    selectedProduct = '';
 }
