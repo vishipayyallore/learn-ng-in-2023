6875b5c78189c64b3096535139b7ba7ae4e94afc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-10 21:57:18 +0530

    Update product-list.component.ts

--- shop-app/src/app/products/product-list/product-list.component.ts
+++ shop-app/src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
     selector: 'sv-product-list',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
     // selectedProduct = '';
     selectedProduct = 'Microphone';
 
+    @ViewChild(ProductDetailComponent) productDetail:
+        | ProductDetailComponent
+        | undefined;
+
     onBuy(name: string) {
         window.alert(`You just bought ${name}!`);
+    }
+
+    ngAfterViewInit(): void {
+        if (this.productDetail) {
+            console.log(this.productDetail.name);
+        }
     }
 }
