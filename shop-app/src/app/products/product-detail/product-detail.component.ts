08275715449307b9743a1789ad402531b2b43f1e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-10 21:55:05 +0530

    Update product-detail.component.ts

--- shop-app/src/app/products/product-detail/product-detail.component.ts
+++ shop-app/src/app/products/product-detail/product-detail.component.ts
@@ -5,6 +5,8 @@ import {
     Output,
     EventEmitter,
     ViewEncapsulation,
+    OnChanges,
+    SimpleChanges,
     ChangeDetectionStrategy,
 } from '@angular/core';
 
@@ -15,7 +17,7 @@ import {
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnChanges {
     @Input() name = '';
 
     @Output() bought = new EventEmitter<string>();
@@ -30,6 +32,16 @@ export class ProductDetailComponent implements OnInit {
         console.log(
             `Name is ${this.name} in the ProductDetailComponent::ngOnInit`
         );
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        const product = changes['name'];
+
+        if (!product.isFirstChange()) {
+            const oldValue = product.previousValue;
+            const newValue = product.currentValue;
+            console.log(`Product changed from ${oldValue} to ${newValue}`);
+        }
     }
 
     buy() {
