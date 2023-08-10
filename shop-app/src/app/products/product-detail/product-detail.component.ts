e1ccc66626e417314fd9ffcbf6800614195e101b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-10 07:58:32 +0530

    Swamy/07aug chapter 03 (#20)
    * Initial version
    
    * Changes
    
    * Products Module
    
    * Update app.module.ts
    
    * Products List
    
    * Update README.md
    
    * Changes
    
    * Update app.component.html
    
    * Product Details
    
    * Product List
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Update product-detail.component.ts

--- shop-app/src/app/products/product-detail/product-detail.component.ts
+++ shop-app/src/app/products/product-detail/product-detail.component.ts
@@ -1,16 +1,35 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+    Component,
+    Input,
+    OnInit,
+    Output,
+    EventEmitter,
+    ViewEncapsulation,
+    ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
     selector: 'sv-product-detail',
     templateUrl: './product-detail.component.html',
     styleUrls: ['./product-detail.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent {
     @Input() name = '';
 
     @Output() bought = new EventEmitter<string>();
 
+    constructor() {
+        console.log(`Name is ${this.name} in the constructor`);
+    }
+
     buy() {
         this.bought.emit(this.name);
+    }
+
+    get productName(): string {
+        console.log(`Get ${this.name}`);
+        return this.name;
     }
 }
