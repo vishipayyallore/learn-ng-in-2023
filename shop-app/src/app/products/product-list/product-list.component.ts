202a5f50bb69d3ed1516bf6c2f0833455d383ae3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-14 16:47:01 +0530

    Changes

--- shop-app/src/app/products/product-list/product-list.component.ts
+++ shop-app/src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductInterface } from '../product-interface';
 
 @Component({
     selector: 'sv-product-list',
@@ -8,22 +9,34 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
     styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements AfterViewInit {
-    selectedProduct = '';
-    // selectedProduct = 'Microphone';
-    products = ['Webcam', 'Microphone', 'Wireless keyboard'];
+    selectedProduct: ProductInterface | undefined;
+
+    products: ProductInterface[] = [
+        {
+            name: 'Webcam',
+            price: 100,
+        },
+        {
+            name: 'Microphone',
+            price: 200,
+        },
+        {
+            name: 'Wireless keyboard',
+            price: 85,
+        },
+    ];
 
     @ViewChild(ProductDetailComponent) productDetail:
         | ProductDetailComponent
         | undefined;
 
-    onBuy(name: string) {
-        window.alert(`You just bought ${name}!`);
-    }
-
     ngAfterViewInit(): void {
         if (this.productDetail) {
-            console.log(this.productDetail.name);
+            console.log(this.productDetail.product);
         }
+    }
+    onBuy() {
+        window.alert('You just bought ${this.selectedProduct?.name}!');
     }
 
     trackByProducts(index: number, name: string): string {
