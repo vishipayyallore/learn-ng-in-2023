1f5a9ed5b217fd6c2734af10b251acd57b88e535
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-14 19:24:20 +0530

    Update app.module.ts

--- shop-app/src/app/app.module.ts
+++ shop-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { FilterPipe } from './products/filter.pipe';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ProductsModule],
+    imports: [BrowserModule, FilterPipe, AppRoutingModule, ProductsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
