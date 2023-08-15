b3920d85a4c4c58b9685a8eaa36d95702c431970
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-15 12:40:07 +0530

    Changes

--- shop-app/src/app/permission.directive.ts
+++ shop-app/src/app/permission.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import {
+    Directive,
+    Input,
+    TemplateRef,
+    ViewContainerRef,
+    OnInit,
+} from '@angular/core';
 
 @Directive({
-  selector: '[svPermission]'
+    selector: '[svPermission]',
 })
-export class PermissionDirective {
-
-  constructor() { }
-
+export class PermissionDirective implements OnInit {
+    @Input() svPermission: string[] = [];
+    private currentRole = 'agent';
+    constructor(
+        private tmplRef: TemplateRef<any>,
+        private vc: ViewContainerRef
+    ) {}
+    ngOnInit(): void {
+        if (this.svPermission.indexOf(this.currentRole) === -1) {
+            this.vc.clear();
+        } else {
+            this.vc.createEmbeddedView(this.tmplRef);
+        }
+    }
 }
