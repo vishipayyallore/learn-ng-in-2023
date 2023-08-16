3df76a0ac4b669f3ec15556a85f0df801e3bef2a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-16 20:53:02 +0530

    Update products.service.ts

--- shop-app/src/app/products/products.service.ts
+++ shop-app/src/app/products/products.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
+import { ProductInterface } from './product-interface';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ProductsService {
+    constructor() {}
 
-  constructor() { }
+    getProducts(): ProductInterface[] {
+        return [
+            {
+                name: 'Webcam',
+                price: 100,
+            },
+            {
+                name: 'Microphone',
+                price: 200,
+            },
+            {
+                name: 'Wireless keyboard',
+                price: 85,
+            },
+        ];
+    }
 }
