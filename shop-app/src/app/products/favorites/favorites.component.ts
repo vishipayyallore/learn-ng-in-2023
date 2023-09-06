cbf65e846d6d1ca498a111b4b0bb54c103c21916
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-06 15:53:57 +0530

    Update favorites.component.ts

--- shop-app/src/app/products/favorites/favorites.component.ts
+++ shop-app/src/app/products/favorites/favorites.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+
 import { ProductInterface } from '../product-interface';
 import { ProductsService } from '../products.service';
+import { FavoritesService } from './favorites.service';
 
 @Component({
   selector: 'sv-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
+  providers: [
+    { provide: ProductsService, useClass: FavoritesService }
+  ]
 })
 export class FavoritesComponent implements OnInit {
   products: ProductInterface[] = [];
