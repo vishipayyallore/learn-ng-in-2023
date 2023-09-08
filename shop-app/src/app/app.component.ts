fd6c97b46b5b11ccd8a6985b64eaf8cba23f2709
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-08 16:02:34 +0530

    Swamy/08sep chapter6 day5 (#32)
    * Formatting
    
    * Update package-lock.json
    
    * Formatting
    
    * Formatting
    
    * Changes
    
    * Update favorites.service.ts
    
    * Changes

--- shop-app/src/app/app.component.ts
+++ shop-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, Inject } from '@angular/core';
+import { APP_CONFIG, appSettings, AppConfig } from './app.config';
 
 @Component({
     selector: 'sv-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    providers: [{ provide: APP_CONFIG, useValue: appSettings }],
 })
 export class AppComponent implements OnDestroy {
     title = 'Shop Application';
+
+    constructor(@Inject(APP_CONFIG) config: AppConfig) { }
 
     ngOnDestroy(): void { }
 }
--- shop-app/src/app/products/favorites/favorites.component.ts
+++ shop-app/src/app/products/favorites/favorites.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductInterface } from '../product-interface';
 import { ProductsService } from '../products.service';
-import { FavoritesService } from '../favorites.service';
+// import { FavoritesService } from '../favorites.service';
+import { favoritesFactory } from '../favorites';
+import { ProductViewService } from '../product-view/product-view.service';
 
 @Component({
   selector: 'sv-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss'],
   providers: [
-    { provide: ProductsService, useClass: FavoritesService }
-  ]
+    { provide: ProductsService, useFactory: favoritesFactory(true), deps: [ProductViewService] }
+  ],
+  // providers: [
+  //   { provide: ProductsService, useClass: FavoritesService }
+  // ]
 })
 export class FavoritesComponent implements OnInit {
   products: ProductInterface[] = [];
