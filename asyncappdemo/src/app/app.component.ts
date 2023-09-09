cc85f308efabaa3d4581e244116bbbf34e96a038
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-09 23:05:34 +0530

    Update app.component.ts

--- asyncappdemo/src/app/app.component.ts
+++ asyncappdemo/src/app/app.component.ts
@@ -6,17 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Learning Angular Async App';
+  title = 'Learning Angular';
 
   constructor() {
+    console.log(`constructor() - title is: ${this.title}`);
     this.changeTitle(this.setTitle);
   }
 
   private setTitle = () => {
     this.title = 'Learning Angular Async App';
+    console.log(`setTitle() - title is: ${this.title}`);
   }
 
   private changeTitle(callback: Function) {
+    console.log(`changeTitle() - title is: ${this.title}`);
     setTimeout(() => {
       callback();
     }, 2000);
