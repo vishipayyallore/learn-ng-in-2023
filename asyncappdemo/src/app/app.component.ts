ccd57e254cb6582adb8bbf3f8a31c9d5a3214a7d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-10 13:33:35 +0530

    Swamy/09sep chapter7 day1 (#33)
    * Initial Version
    
    * Initial Version
    
    * Changes
    
    * Update app.component.ts
    
    * Update app.component.ts
    
    * Update index.html
    
    * Update app.component.ts

---
+++ asyncappdemo/src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'sv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Learning Angular';
+
+  title$ = new Observable(observer => {
+    setInterval(() => {
+      observer.next();
+    }, 2000);
+  });
+
+  constructor() {
+    console.log(`constructor() - title is: ${this.title}`);
+    // this.changeTitle(this.setTitle); // Callback hell
+
+    // Promise Version
+    // this.onComplete().then(this.setTitle);
+
+    // Observable Version
+    this.title$.subscribe(this.setTitle);
+  }
+
+  private setTitle = () => {
+    this.title = `Learning Angular Async App (${new Date().getMilliseconds()})`;
+
+    console.log(`setTitle() - title is: ${this.title}`);
+  }
+
+  private onComplete() {
+    return new Promise<void>(resolve => {
+      setInterval(() => {
+        resolve();
+      }, 2000);
+    });
+  }
+
+  // private changeTitle(callback: Function) {
+  //   console.log(`changeTitle() - title is: ${this.title}`);
+  //   setTimeout(() => {
+  //     callback();
+  //   }, 2000);
+  // }
+
+  // private onComplete() {
+  //   return new Promise<void>(resolve => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 2000);
+  //   });
+  // }
+
+
+}
