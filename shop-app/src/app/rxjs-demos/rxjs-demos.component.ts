488da84e963c84f0b1ec2b48ebb460ccc8166cd5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-22 21:15:28 +0530

    RxJs Demos

--- shop-app/src/app/rxjs-demos/rxjs-demos.component.ts
+++ shop-app/src/app/rxjs-demos/rxjs-demos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'sv-rxjs-demos',
   templateUrl: './rxjs-demos.component.html',
   styleUrls: ['./rxjs-demos.component.scss']
 })
-export class RxjsDemosComponent {
+export class RxjsDemosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    const values = of(1, 2, 3);
+    values.subscribe(value => console.log(value));
+
+    const values1 = from([4, 5, 6]);
+    values1.subscribe(value => console.log(value));
+  }
+
 
 }
